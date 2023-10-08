f2ab99b4ab6cd09c619bd60eb78c9c00726eeb16
scottmotion <scott@scottmotion.com>
2023-10-08 12:35:42 -0700

    move slider js

--- slider.js
+++ slider.js
@@ -1,6 +1,37 @@
+$(document).ready(function () {
+  $(".work-carousel").slick({
+    arrows: true,
+    centerMode: true,
+    centerPadding: "360px",
+    initialSlide: 2,
+    slidesToShow: 1,
+    variableWidth: true,
+    responsive: [
+      // {
+      //   breakpoint: 1400,
+      //   settings: {
+      //     centerMode: true,
+      //     slidesToShow: 1,
+      //     variableWidth: true,
+      //   },
+      // },
 
-$(document).ready(function(){
-  $('.your-class').slick({
-    setting-name: setting-value
+      {
+        breakpoint: 768,
+        settings: {
+          centerPadding: "44px",
+          variableWidth: false,
+        },
+      },
+      // {
+      //   breakpoint: 376,
+      //   settings: {
+      //     centerMode: true,
+      //     centerPadding: "44px",
+      //     slidesToShow: 1,
+      //     variableWidth: false,
+      //   },
+      // },
+    ],
   });
 });
